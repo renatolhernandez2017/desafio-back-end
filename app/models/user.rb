5af839fab56a4eb58081f7e848b307e1ca3207b0
Renato Hernandez <renatolhernandez@hotmail.com>
2026-01-16 11:50:10 -0300

    adicionar a ligação com entities e ajustar email com case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  belongs_to :account
+  has_many :entity_users
+  has_many :entities, through: :entity_users
+  
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 
   after_create :send_welcome_email
 
