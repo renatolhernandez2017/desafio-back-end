dcba82bb61cfe772c17dc3f6b88703fd67c94464
Renato Hernandez <renatolhernandez@hotmail.com>
2026-01-16 11:50:40 -0300

    Refatorar o create account

--- app/services/create_account.rb
+++ app/services/create_account.rb
@@ -1,56 +1,54 @@
 class CreateAccount < ApplicationService
-  def initialize(payload, from_fintera = false)
-    @payload = payload
-    @from_fintera = from_fintera
-    @errors = []
+  def initialize(params)
+    @params = params
   end
 
   def call
-    if !is_account_valid?
-      @errors << "Account is not valid"
-      Result.new(false, nil, @errors.join(","))
-    else
-      account = Account.new(account_params)
-      if account.save && User.insert_all(users_params(account))
-        Result.new(true, account)
-      else
-        @errors << account.errors.full_messages
-        Result.new(false, nil, @errors.join(","))
-      end
+    ActiveRecord::Base.transaction do
+      account = create_account!
+      create_entities!(account)
+      account
     end
   end
 
-  def is_account_valid?
-    return false if @payload.blank?
+  private
 
-    true
+  def create_account!
+    Account.create!(account_params)
+  end
+
+  def create_entities!(account)
+    raise ActiveRecord::RecordInvalid, account if entities_params.blank?
+
+    entities_params.each do |entity_params|
+      entity = account.entities.create!(name: entity_params[:name])
+      associate_users!(entity, entity_params[:users])
+    end
+  end
+
+  def associate_users!(entity, users)
+    raise ActiveRecord::RecordInvalid, entity if users.blank?
+
+    Array(users).each do |user_params|
+      user = find_or_create_user!(user_params)
+
+      entity.users << user unless entity.users.exists?(user.id)
+    end
+  end
+
+  def find_or_create_user!(params)
+    User.find_or_create_by!(email: params[:email]) do |user|
+      user.first_name = params[:first_name]
+      user.last_name  = params[:last_name]
+      user.phone      = params[:phone]
+    end
   end
 
   def account_params
-    if @from_fintera
-      {
-        name: @payload[:name],
-        active: true,
-      }
-    else
-      {
-        name: @payload[:name],
-        active: false,
-      }
-    end
+    @params.slice(:name)
   end
 
-  def users_params(account)
-    @payload[:users].map do |user|
-      {
-        first_name: user[:first_name],
-        last_name: user[:last_name],
-        email: user[:email],
-        phone: user[:phone].to_s.gsub(/\D/, ""),
-        account_id: account.id,
-        created_at: Time.zone.now,
-        updated_at: Time.zone.now,
-      }
-    end
+  def entities_params
+    @params[:entities] || []
   end
 end
